20395ff0edbf508fb1dc32c0cf9afb3e02539a02
lspaschoal <42151884+lspaschoal@users.noreply.github.com>
2023-01-06 18:17:05 -0300

    Update script.js
    adicionada integração com API do redemet e exibição de TAF

--- script.js
+++ script.js
@@ -1,31 +1,139 @@
 // Grupos de localidades. Cada grupo gera um fieldset com o nome da propriedade, onde são criados os inputs para cada aerodromo
 
-const briefing3 = {
-    'tma-rj': ['sbgl.gig', 'sbrj.sdu'],
-    'tma-sp': ['sbgr.gru', 'sbsp.cgh', 'sbkp.vcp'],
-    'fir-az': ['sbsl.slz', 'sbiz.imp', 'sbbe.bel', 'sbsn.stm', 'sbma.mab', 'sbmq.mcp', 'sbeg.mao',
-        'sbbv.bvb', 'sbpv.pvh', 'sbrb.rbr', 'sbcy.cgb','sbsi.ops'
+const briefing = {
+    'tma-rj': [
+        'SBGL.GIG',
+        'SBRJ.SDU'
     ],
-    'fir-re': ['sbvt.vix', 'sbps.bps', 'sbil.ios', 'sbtc.una', 'sbvc.vdc', 'sbsv.ssa', 'sbar.aju', 'sbmo.mcz',
-        'sbrf.rec', 'sbpl.pnz', 'sbjp.jpa', 'sbsg.nat', 'sbfz.for', 'sbju.jdo','sbje.jjd','sbte.the'
+    'tma-sp': [
+        'SBGR.GRU',
+        'SBSP.CGH',
+        'SBKP.VCP'
     ],
-    'fir-bs': ['sbpj.pmw', 'sbbr.bsb', 'sbgo.gyn', 'sbcn.clv', 'sbcf.cnf', 'sbul.udi', 'sbmk.moc', 'sbrp.rao',
-        'sbsr.sjp', 'sbae.bau', 'sdsc.qsc'
+    'fir-az': [
+        'SBSL.SLZ',
+        'SBIZ.IMP',
+        'SBBE.BEL',
+        'SBSN.STM',
+        'SBMA.MAB',
+        'SBHT.ATM',
+        'SBCJ.CKS',
+        'SBMQ.MCP',
+        'SBEG.MAO',
+        'SBTT.TBT',
+        'SBTF.TFF',
+        'SBBV.BVB',
+        'SBPV.PVH',
+        'SBRB.RBR',
+        'SBCZ.CZS',
+        'SBCY.CGB',
+        'SBSI.OPS',
+        'SBRD.ROO',
+        'SBAT.AFL'
     ],
-    'fir-cw': ['sbdn.ppb', 'sbzm.iza', 'sbpa.poa', 'sbcx.cxj', 'sbfl.fln', 'sbnf.nvt', 'sbjv.joi', 'sbch.xap', 'sbja.jjg',
-        'sbct.cwb', 'sbfi.igu', 'sblo.ldb', 'sbmg.mgf', 'sbca.cac', 'sbcg.cgr'
+    'fir-re': [
+        'SBVT.VIX',
+        'SBPS.BPS',
+        'SBIL.IOS',
+        'SBTC.UMA',
+        'SBVC.VDC',
+        'SBSV.SSA',
+        'SBAR.AJU',
+        'SBMO.MCZ',
+        'SBRF.REC',
+        'SBFN.FEN',
+        'SBPL.PNZ',
+        'SBJP.JPA',
+        'SBKG.CPV',
+        'SBSG.NAT',
+        'SBFZ.FOR',
+        'SBJU.JDO',
+        'SBJE.JJD',
+        'SBAC.ARX',
+        'SBTE.THE',
+        'SBPB.PHB'
     ],
-    'argentina': ['saez.eze', 'sabe.aep'],
-    'uruguai': ['sumu.mvd', 'suls.pdp']
+    'fir-bs': [
+        'SBBR.BSB',
+        'SBGO.GYN',
+        'SBPJ.PMW',
+        'SBCN.CLV',
+        'SBCF.CNF',
+        'SBBH.PLU',
+        'SBLS.SBLS',
+        'SBUL.UDI',
+        'SBUR.UBA',
+        'SBMK.MOC',
+        'SBRP.RAO',
+        'SBSR.SJP',
+        'SBAE.JTC',
+        'SDSC.QSC',
+    ],
+    'fir-cw': [
+        'SBME.MEA',
+        'SBDN.PPB',
+        'SBZM.IZA',
+        'SBPA.POA',
+        'SBCX.CXJ',
+        'SBUG.URG',
+        'SBBG.BGX',
+        'SBPK.PET',
+        'SBFL.FLN',
+        'SBNF.NVT',
+        'SBJV.JOI',
+        'SBCH.XAP',
+        'SBJA.JJG',
+        'SBCT.CWB',
+        'SBBI.BFH',
+        'SBFI.IGU',
+        'SBLO.LDB',
+        'SBMG.MGF',
+        'SBCA.CAC',
+        'SBCG.CGR',
+        'SBDB.BYO',
+        'SBDO.DOU',
+    ],
+    'argentina': [
+        'SAEZ.EZE',
+        'SABE.AEP'],
+    'uruguai': [
+        'SUMU.MVD',
+        'SULS.PDP'
+    ]
 }
 // Cria o array para iteração dos grupos e aeródromos
-const GRUPOS = Object.entries(briefing3);
+const GRUPOS = Object.entries(briefing);
 
 // Métodos úteis na exibição dos dados
 // Retorna o codigo icao de uma localidade selecionada
-const icao = localidade => localidade.substring(0, 4).toUpperCase();
+const icao = localidade => localidade.substring(0, 4);
 // Retorna o codigo iata de uma localidade selecionada
-const iata = localidade => localidade.substring(5).toUpperCase();
+const iata = localidade => localidade.substring(5);
+
+// Retorna a lista de ICAOs cadastrados
+const getAllICAO = () => {
+    let icaos = [];
+    for (let ads of Object.values(briefing)) {
+        let aerodromos = ads.map(item => icao(item));
+        icaos = [...icaos, ...aerodromos];
+    }
+    return icaos;
+}
+
+function getData(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then((resp) => resp.json())
+            .then((data) => {
+                resolve(data);
+            });
+    })
+}
+
+// Edita os TAFS para exibição
+const tabulaTAF = (taf) => {
+    return taf.replace('TX',',TX').replaceAll('BECMG',',BECMG').replaceAll('TEMPO',',TEMPO').split(',');
+}
 
 // Gera os campos para inserir a informação
 const gerarCampos = function () {
@@ -35,22 +143,52 @@ const gerarCampos = function () {
         let leg = document.createElement('legend');
         leg.textContent = GRUPOS[i][0].toUpperCase();
         fs.appendChild(leg);
-        document.getElementById('coordenacao').appendChild(fs);
-        // Lendo cada aerodromo do grupos e criando uma label e um input pra cada um deles
-        for (let j = 0; j < GRUPOS[i][1].length; j++) {
-            //Cria uma div pra cada aerodromo
-            let div = document.createElement('div');
-            fs.appendChild(div);
-            // Cria a label de cada aerodromo
-            let label = document.createElement('label');
-            label.textContent = icao(GRUPOS[i][1][j]);
-            div.appendChild(label);
-            // Cria o input de cada aerodromo
-            let input = document.createElement('input');
-            input.type = 'text';
-            input.id = GRUPOS[i][1][j];
-            div.appendChild(input);
-        }
+        document.getElementById('formulario').appendChild(fs);
+
+        let requisicoes = [];
+
+        // Gerando as urls para as promises
+        getAllICAO().forEach(icao => {
+            requisicoes.push(getData(`https://api-redemet.decea.mil.br/mensagens/taf/${icao}?api_key=6vmvTQDP1t8thEEAUkCCj4z4TRjrJLcb561p1SRi`));
+        });
+
+        // Buscando os TAF no REDEMET
+        Promise.all(requisicoes)
+            .then(dados => {
+                let mensagens = new Map();
+                dados.forEach(resposta => {
+                    if (resposta.data.data.length > 0) mensagens.set(resposta.data.data[0].id_localidade,resposta.data.data[0].mens);
+                });
+                // Lendo cada aerodromo do array grupos e criando uma label e um input pra cada um deles
+                for (let j = 0; j < GRUPOS[i][1].length; j++) {
+                    //Cria uma div pra cada aerodromo
+                    let div = document.createElement('div');
+                    div.classList.add('div_ad');
+                    fs.appendChild(div);
+                    // Cria a div com o TAF
+                    let div_taf = document.createElement('div');
+                    div_taf.classList.add('div_taf');
+                    
+                    if (mensagens.has(icao(GRUPOS[i][1][j]))) {
+                        let msg = tabulaTAF(mensagens.get(icao(GRUPOS[i][1][j])));
+                        msg.forEach(segmento => (msg.indexOf(segmento) > 0) ? div_taf.innerHTML += segmento + '<br>' : div_taf.innerHTML += segmento);
+                    } else {
+                        div_taf.textContent = `Mensagem TAF de ${icao(GRUPOS[i][1][j])} não localizada na base de dados da REDEMET`;
+                        div_taf.style.setProperty('color','red');
+                    }
+                    div.appendChild(div_taf);
+                    // Cria a label de cada aerodromo
+                    let label = document.createElement('label');
+                    label.textContent = icao(GRUPOS[i][1][j]);
+                    label.classList.add('label_icao')
+                    div.appendChild(label);
+                    // Cria o input de cada aerodromo
+                    let input = document.createElement('input');
+                    input.type = 'text';
+                    input.id = GRUPOS[i][1][j];
+                    div.appendChild(input);
+                }
+            });
     }
 }
 // Chamada da função ao carregar a página
@@ -116,13 +254,13 @@ const imprimeBriefing = function () {
             let condicoes_localidades = Object.entries(briefing[GRUPOS[i][0]]);
             for (let j = 0; j < condicoes_localidades.length; j++) {
                 let p = document.createElement('p');
+                p.textContent = 'Previsão de ' + condicoes_localidades[j][0] + ' para ';
                 for (let k = 0; k < condicoes_localidades[j][1].length; k++) {
                     p.textContent += iata(condicoes_localidades[j][1][k]);
                     if (k < condicoes_localidades[j][1].length - 1) {
                         p.textContent += '/';
                     }
                 }
-                p.textContent += ' - ' + condicoes_localidades[j][0] + ';';
                 td2.appendChild(p);
             }
         }
